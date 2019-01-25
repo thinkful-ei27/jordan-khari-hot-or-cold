bbd8260f202571bde39bce5e5d8a10aa8f54b9b8
Jordan Heffernan <jheff2@gmail.com>
2019-01-25 09:50:16 -0800

    created functioning response, began work on grabbing guess value

--- src/components/game.js
+++ src/components/game.js
@@ -2,17 +2,61 @@ import React from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
+import GuessCount from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
-}
 
+const feedback = [
+  'Make your guess',
+  'Hot',
+  'Cold',
+  'You Got It!'
+]
+
+export default class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      guesses: [],
+      currentGuess: 0,
+      answer: Math.floor(Math.random() * 101) + 1,
+    }
+  }
+
+  setFeedback() {
+    let guess = this.state.currentGuess;
+    let answer = this.state.answer
+    console.log(answer)
+    let spread = Math.abs(guess - answer)
+    console.log(spread)
+    if (guess === 0) {
+      return feedback[0]
+    } else if (spread === 0) {
+      return feedback[3]
+    } else if (spread > 50) {
+      return feedback[2]
+    } else {
+      return feedback[1]
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({currentGuess: event.taget.value});
+    console.log(this.state.currentGuess);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <GuessSection feedback={this.setFeedback()} value={this.state.currentGuess} onSubmit={event => this.handleSubmit(event)} onChange={event => this.handleChange(event)}/>
+        <GuessCount count={this.state.guesses.length} />
+        <GuessList guesses={this.state.guesses} />
+      </div>
+    );
+  }
+}
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -4,10 +4,10 @@ import './guess-form.css';
 
 export default function GuessForm(props) {
     return (
-        <form>
+        <form onSubmit={props.onSubmit}>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
+                placeholder="Enter your Guess" value={props.value} required onChange={props.onChange}/>
             <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
         </form>
     );
